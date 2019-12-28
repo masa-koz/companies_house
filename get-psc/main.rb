99984bcbfdd672f419c9b39af99403995ba4a951
Masahiro Kozuka <masa.koz@gmail.com>
2019-12-27 22:58:27 -0500

    use json.

--- get-psc/main.rb
+++ get-psc/main.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require 'csv'
+require 'json'
 require 'mongo'
-
-client = Mongo::Client.new(['127.0.0.1:27017'],
-                           database: 'uk_companies', monitoring: false)
-collection = client[:psc]
-collection_basic = client[:BasicCompanyData]
+require 'optparse'
 
 def write_into_csv(result)
   data = result['data']
@@ -33,105 +29,100 @@ def write_into_csv(result)
    voting, voting_kind].to_csv
 end
 
-def write_basic_into_csv(company_number, tier, result, psc_info)
-  individual_ultimate_psc =
-    if psc_info.key?('ultimate_psc')
-      psc_info['ultimate_psc'].include?('individual-person-with-significant-control')
-    else
-      false
+def get_subsidiaries(collection, psc_country)
+  subsidiaries = {}
+
+  tier = 1
+  subsidiaries[tier] = {}
+  warn "Tier#{tier}:"
+  collection.find('data.address.country': psc_country,
+                  'data.ceased_on': { '$exists': false }).each do |result|
+    unless subsidiaries[tier].key?(result['company_number'])
+      subsidiaries[tier][result['company_number']] = { ultimate_psc_country: psc_country, has_individual_ultimate_psc: false }
     end
-  t1_psc = psc_info['t1_psc']
-
-  if result.nil?
-    [company_number, nil, nil, individual_ultimate_psc, t1_psc, nil, nil].to_csv
-  else
-    accounts = result['Accounts']
-    [company_number, result['CompanyName'], tier,
-     individual_ultimate_psc, t1_psc,
-     accounts['AccountCategory'], accounts['AccountRefMonth'],
-     accounts['AccountRefDay']].to_csv
-  end
-end
-
-subsidiaries = {}
-
-cvs_file = open('psc.csv', 'w')
-cvs_basic_file = open('basic.csv', 'w')
-
-tier = 1
-subsidiaries[tier] = {}
-warn "Tier#{tier}:"
-collection.find('data.address.country': 'Japan',
-                'data.ceased_on': { '$exists': false }).each do |result|
-  cvs_file.puts write_into_csv(result)
-  STDOUT.flush
-  unless subsidiaries[tier].key?(result['company_number'])
-    subsidiaries[tier][result['company_number']] = { 'ultimate_psc' => [] }
-  end
-  ultimate_psc_info =
-    subsidiaries[tier][result['company_number']]['ultimate_psc']
-  ultimate_psc_info.push(result.dig('data', 'kind'))
-  p ultimate_psc_info
-end
-
-warn "Number of new Tier#{tier}: #{subsidiaries[tier].size}"
-tier += 1
-subsidiaries[tier] = {}
-
-until subsidiaries[tier - 1].empty?
-  warn "Try to find Tier#{tier} subsidiaries by Tier#{tier - 1} " \
-       "subsidiaries(Number of new: #{subsidiaries[tier - 1].size}):"
-  subsidiaries[tier - 1].keys.each_with_index do |company_number, i|
-    warn "(#{i + 1}/#{subsidiaries[tier - 1].keys.size}) " \
-         'Try to find a company which is controlled by' \
-         " #{company_number}."
-
-    collection.find('data.identification.registration_number': company_number,
-                    'data.kind':
-                      'corporate-entity-person-with-significant-control',
-                    'data.ceased_on':
-                      { '$exists': false }).each do |result|
-      warn 'FOUND'
-      cvs_file.puts write_into_csv(result)
-      STDOUT.flush
-
-      unless subsidiaries[tier].key?(result['company_number'])
-        subsidiaries[tier][result['company_number']] = { 't1_psc' => [] }
-      end
-      if subsidiaries[tier - 1][company_number].key('t1_psc')
-        subsidiaries[tier][result['company_number']]['t1_psc'] =
-          subsidiaries[tier - 1][company_number]['t1_psc']
-      else
-        subsidiaries[tier][result['company_number']]['t1_psc'] =
-          company_number
-      end
+    if result.dig('data', 'kind') == 'individual-person-with-significant-control'
+      subsidiaries[tier][result['company_number']][:has_individual_ultimate_psc] = true
     end
   end
-
-  # Exclude the already listed up subsidiaries
-  new_subsidiaries_keys = subsidiaries[tier].keys.dup
-  1.step(tier - 1) do |x|
-    new_subsidiaries_keys -= subsidiaries[x].keys
-  end
-  subsidiaries[tier] = subsidiaries[tier].slice(*new_subsidiaries_keys)
 
   warn "Number of new Tier#{tier}: #{subsidiaries[tier].size}"
   tier += 1
   subsidiaries[tier] = {}
+
+  until subsidiaries[tier - 1].empty?
+    # warn "Try to find Tier#{tier} subsidiaries by Tier#{tier - 1} " \
+    #     "subsidiaries(Number of new: #{subsidiaries[tier - 1].size}):"
+    subsidiaries[tier - 1].keys.each_with_index do |company_number, _i|
+      # warn "(#{i + 1}/#{subsidiaries[tier - 1].keys.size}) " \
+      #     'Try to find a company which is controlled by' \
+      #     " #{company_number}."
+
+      collection.find('data.identification.registration_number': company_number,
+                      'data.kind':
+                        'corporate-entity-person-with-significant-control',
+                      'data.ceased_on':
+                        { '$exists': false }).each do |result|
+        # warn 'FOUND'
+
+        unless subsidiaries[tier].key?(result['company_number'])
+          subsidiaries[tier][result['company_number']] = { ultimate_psc_country: psc_country }
+        end
+
+        if subsidiaries[tier - 1][company_number].key(:t1_psc)
+          subsidiaries[tier][result['company_number']][:t1_psc] =
+            subsidiaries[tier - 1][company_number][:t1_psc]
+        else
+          subsidiaries[tier][result['company_number']][:t1_psc] =
+            company_number
+        end
+        subsidiaries[tier][result['company_number']][:has_individual_ultimate_psc] =
+          subsidiaries[tier - 1][company_number][:has_individual_ultimate_psc]
+      end
+    end
+
+    # Exclude the already listed up subsidiaries
+    new_subsidiaries_keys = subsidiaries[tier].keys.dup
+    1.step(tier - 1) do |x|
+      new_subsidiaries_keys -= subsidiaries[x].keys
+    end
+    subsidiaries[tier] = subsidiaries[tier].slice(*new_subsidiaries_keys)
+
+    warn "Number of new Tier#{tier}: #{subsidiaries[tier].size}"
+    tier += 1
+    subsidiaries[tier] = {}
+  end
+  subsidiaries
 end
 
-1.step(tier - 1) do |x|
-  subsidiaries[x].each do |company_number, psc_info|
-    # Transform company_number as number when it is saved as that on DB.
-    result = if company_number =~ /^\h+$/
-               collection_basic.find('CompanyNumber': company_number.to_i).first
-             else
-               collection_basic.find('CompanyNumber': company_number).first
-             end
-    warn "Cannot find any basic info for #{company_number}" if result.nil?
-    cvs_basic_file.puts write_basic_into_csv(company_number, x, result, psc_info)
+opt = OptionParser.new
+params = {}
+opt.on('-C [VAL]') { |v| params[:C] = v }
+opt.on('-j [VAL]') { |v| params[:j] = v }
+opt.parse!(ARGV)
+
+psc_country = if params.key?(:C)
+                params[:C]
+              else
+                'Japan'
+end
+
+jsonname = if params.key?(:j)
+             params[:j]
+           else
+             'japanese_subsidiaries.json'
+end
+
+client = Mongo::Client.new(['127.0.0.1:27017'],
+                           database: 'uk_companies', monitoring: false)
+subsidiaries = get_subsidiaries(client[:psc], psc_country)
+
+output = open(jsonname, 'w')
+subsidiaries.each do |tier, sub_subsidiaries|
+  sub_subsidiaries.each do |company_number, info|
+    json = { registered_number: company_number, tier: tier, t1_psc: info[:t1_psc],
+      ultimate_psc: {country: info[:ultimate_psc_country], has_individual: info[:has_individual_ultimate_psc] }}
+    output.puts json.to_json
   end
 end
 
-cvs_file.close
-cvs_basic_file.close
+output.close
