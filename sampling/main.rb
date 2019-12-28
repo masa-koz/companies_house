a649221a23b14a50d44eaed0b2770825f0333139
Masahiro Kozuka <masa.koz@gmail.com>
2019-12-28 17:59:03 -0500

    add the recursive mode.

--- sampling/main.rb
+++ sampling/main.rb
@@ -1,51 +1,94 @@
 # frozen_string_literal: true
 
+require 'csv'
+require 'enumerable/statistics'
 require 'json'
+require 'matrix'
 require 'mongo'
 require 'optparse'
 
 opt = OptionParser.new
 params = {}
 opt.on('-a [VAL]') { |v| params[:a] = v }
-opt.on('-j [VAL]') { |v| params[:j] = v }
+opt.on('-o [VAL]') { |v| params[:o] = v }
+opt.on('-r') { |v| params[:r] = v }
+opt.on('-s [VAL]') { |v| params[:s] = v }
 opt.parse!(ARGV)
 
 all_jsonname = params[:a]
-jsonname = params[:j]
+jsonname = params[:s]
+csvname = params[:o]
 
-all_output = (open(all_jsonname, 'w') if all_jsonname)
-
-output = if jsonname
-           open(jsonname, 'w')
-         else
-           STDOUT
-         end
 client = Mongo::Client.new(['127.0.0.1:27017'],
                            database: 'uk_companies', monitoring: false)
 
-collection = client[:BasicCompanyData]
+csv_output = if csvname
+               open(csvname, 'w')
+             else
+               STDOUT
+             end
 
-random = Random.new(2)
-
-results = collection.find('Accounts.AccountCategory' => 'FULL').projection('CompanyNumber' => 1)
-
-all = []
-sampling = []
-
-warn "Number of active companies: #{results.count}"
-results.each do |result|
+registered_numbers = Hash[client[:BasicCompanyData].find(
+  'Accounts.AccountCategory' => 'FULL'
+).projection('CompanyNumber' => 1).each_with_index.collect do |result, i|
   registered_number = if m = /^0*([1-9]+\d*)$/.match(result.dig('CompanyNumber').to_s)
                         '0' * (8 - m[1].size) + (m[1]).to_s
                       else
                         result.dig('CompanyNumber').to_s
-                      end
-  all.push(registered_number)
-  next if random.rand(1..100) < 99
+  end
+  [registered_number, i]
+end]
 
-  sampling.push(registered_number)
+warn "Number of active companies: #{registered_numbers.keys.size}"
+
+subsidiaries =
+  if params[:r]
+    _subsidiaries = client[:subsidiaries].find.projection('registered_number' => 1, 'ultimate_psc.country' => 1)
+    Hash[_subsidiaries.collect { |v| [v.dig('registered_number'), v.dig('ultimate_psc', 'country')] }]
+  else
+    {}
+  end
+
+count = 0
+
+mean = registered_numbers.values.mean
+csv = [count, mean].flatten(1).to_csv
+csv_output.puts csv
+csv_output.flush
+warn csv
+count += 1
+
+count.step(100_000) do
+  warn "#{count}:"
+  output = if jsonname && params[:r].nil?
+             open("#{jsonname}_#{count}", 'w')
+           else
+             STDOUT
+           end
+
+  sample = registered_numbers.keys.sample(registered_numbers.size / 50)
+
+  # sample_subsidiaries = registered_numbers.slice(*(sample & subsidiaries.keys))
+  sample_registered_numbers = registered_numbers.slice(*sample)
+
+  mean = sample_registered_numbers.values.mean
+  csv = [count, mean].flatten(1).to_csv
+  csv_output.puts csv
+  csv_output.flush
+  warn csv
+
+  warn "Number of sample companies: #{sample.size}"
+
+  output.puts sample.to_json
+  output.close
+
+  break if params[:r].nil?
 end
 
-warn "Number of sampled companies: #{sampling.size}"
+csv_output.close
 
-all_output&.puts all.to_json
-output.puts sampling.to_json
+if all_jsonname
+  all_output = open(all_jsonname, 'w')
+  all_output&.puts registered_numbers.to_json
+  all_output.close
+end
